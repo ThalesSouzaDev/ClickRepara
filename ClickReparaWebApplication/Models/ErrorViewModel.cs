893dedee7f8abeac3042533bd88c3aa16f59fce0
Thales <thales_ss@hotmail.com>
2022-02-05 16:46:58 -0300

    Adicionar arquivos de projeto.

---
+++ ClickReparaWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClickReparaWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
